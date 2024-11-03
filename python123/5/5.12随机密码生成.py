8933fb854ad6598c106e43cf719b80dfe7dff759
GWWeny <GWWeny@163.com>
2024-11-03 10:17:49 +0800

    commit and write

--- python123/5/5.12随机密码生成.py
+++ python123/5/5.12随机密码生成.py
@@ -3,7 +3,7 @@ import random
 
 def genpwd(length):
     return ''.join(str(random.randint(0,9)) for _ in range(length))
-
+# python123编译不过，跟数据不符合
 length = eval(input())
 random.seed(17)
 for i in range(3):
